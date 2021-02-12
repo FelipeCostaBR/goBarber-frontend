9325a669edc67f39ac950c88f94dcc3f367abeb1
Felipe <felipejsilvacosta@gmail.com>
2021-02-12 15:25:33 +1100

    Routes

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,6 +3,9 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -11,53 +14,84 @@ import Input from '../../components/Input';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, Background, AnimationContainer } from './styles';
+
+interface SignInFormData {
+    email: string;
+    password: string;
+}
 
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
+    const { signIn } = useAuth();
+    const { addToast } = useToast();
+    const history = useHistory();
 
-    const handleSubmit = useCallback(async (data: object) => {
-        try {
-            formRef.current?.setErrors({});
+    const handleSubmit = useCallback(
+        async (data: SignInFormData) => {
+            try {
+                formRef.current?.setErrors({});
 
-            const schema = Yup.object().shape({
-                email: Yup.string()
-                    .required('Email is required')
-                    .email('Email is required'),
-                password: Yup.string().required('Password is required'),
-            });
+                const schema = Yup.object().shape({
+                    email: Yup.string()
+                        .required('Email is required')
+                        .email('Email incorrect'),
+                    password: Yup.string().required('Password is required'),
+                });
 
-            await schema.validate(data, {
-                abortEarly: false,
-            });
-        } catch (err) {
-            const errors = getValidationErrors(err);
-            formRef.current?.setErrors(errors);
-        }
-    }, []);
+                await schema.validate(data, {
+                    abortEarly: false,
+                });
+
+                await signIn({ email: data.email, password: data.password });
+
+                history.push('/dashboard');
+            } catch (err) {
+                if (err instanceof Yup.ValidationError) {
+                    const errors = getValidationErrors(err);
+                    formRef.current?.setErrors(errors);
+
+                    return;
+                }
+                addToast({
+                    type: 'info',
+                    title: 'Login Error',
+                    description: 'Something wrong when tried to log in',
+                });
+            }
+        },
+        [signIn, addToast, history],
+    );
+
     return (
         <Container>
             <Content>
-                <img src={logoImg} alt="GoBarber" />
+                <AnimationContainer>
+                    <img src={logoImg} alt="GoBarber" />
 
-                <Form ref={formRef} onSubmit={handleSubmit}>
-                    <h1>Login</h1>
-                    <Input name="email" icon={FiMail} placeholder="E-mail" />
-                    <Input
-                        name="password"
-                        icon={FiLock}
-                        type="password"
-                        placeholder="Login"
-                    />
+                    <Form ref={formRef} onSubmit={handleSubmit}>
+                        <h1>Login</h1>
+                        <Input
+                            name="email"
+                            icon={FiMail}
+                            placeholder="E-mail"
+                        />
+                        <Input
+                            name="password"
+                            icon={FiLock}
+                            type="password"
+                            placeholder="Login"
+                        />
 
-                    <Button type="submit"> Entry </Button>
+                        <Button type="submit"> Entry </Button>
 
-                    <a href="forgot">Forgot Password</a>
-                </Form>
-                <a href="Login">
-                    <FiLogIn />
-                    Log in
-                </a>
+                        <a href="forgot">Forgot Password ?</a>
+                    </Form>
+                    <Link to="/signup">
+                        <FiLogIn />
+                        Sign up
+                    </Link>
+                </AnimationContainer>
             </Content>
 
             <Background />
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,62 +3,100 @@ import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-
+import { Link, useHistory } from 'react-router-dom';
 import getValidationErrors from '../../utils/getValidationErrors';
 import logoImg from '../../assets/logo.svg';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, Background, AnimationContainer } from './styles';
+import api from '../../services/api';
+
+import { useToast } from '../../hooks/toast';
+
+interface SignUpFormData {
+    name: string;
+    email: string;
+    password: string;
+}
 
 const SignUp: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
+    const { addToast } = useToast();
+    const history = useHistory();
 
-    const handleSubmit = useCallback(async (data: object) => {
-        try {
-            formRef.current?.setErrors({});
+    const handleSubmit = useCallback(
+        async (data: SignUpFormData) => {
+            try {
+                formRef.current?.setErrors({});
 
-            const schema = Yup.object().shape({
-                name: Yup.string().required('Nome is required'),
-                email: Yup.string()
-                    .required('Email is required')
-                    .email('Email is required'),
-                password: Yup.string().min(6, 'min 6 characters'),
-            });
+                const schema = Yup.object().shape({
+                    name: Yup.string().required('Nome is required'),
+                    email: Yup.string()
+                        .required('Email is required')
+                        .email('Email is required'),
+                    password: Yup.string().min(6, 'min 6 characters'),
+                });
 
-            await schema.validate(data, {
-                abortEarly: false,
-            });
-        } catch (err) {
-            const errors = getValidationErrors(err);
-            formRef.current?.setErrors(errors);
-        }
-    }, []);
+                await schema.validate(data, {
+                    abortEarly: false,
+                });
+
+                await api.post('/users', data);
+                history.push('/');
+
+                addToast({
+                    type: 'success',
+                    title: 'Signup Successful!',
+                    description: 'You can login in GoBarber',
+                });
+            } catch (err) {
+                if (err instanceof Yup.ValidationError) {
+                    const errors = getValidationErrors(err);
+                    formRef.current?.setErrors(errors);
+
+                    return;
+                }
+                addToast({
+                    type: 'error',
+                    title: 'Error to signup ',
+                    description: 'Something wrong when to signup, try again!',
+                });
+            }
+        },
+        [addToast, history],
+    );
 
     return (
         <Container>
             <Background />
             <Content>
-                <img src={logoImg} alt="GoBarber" />
+                <AnimationContainer>
+                    <img src={logoImg} alt="GoBarber" />
 
-                <Form ref={formRef} onSubmit={handleSubmit}>
-                    <h1>Subscribe</h1>
-                    <Input name="name" icon={FiUser} placeholder="Name" />
-                    <Input name="email" icon={FiMail} placeholder="E-mail" />
-                    <Input
-                        name="password"
-                        icon={FiLock}
-                        type="password"
-                        placeholder="Login"
-                    />
-                    <Button type="submit"> Create account </Button>
-                </Form>
+                    <Form ref={formRef} onSubmit={handleSubmit}>
+                        <h1>Subscribe</h1>
+                        <Input name="name" icon={FiUser} placeholder="Name" />
+                        <Input
+                            name="email"
+                            icon={FiMail}
+                            placeholder="E-mail"
+                        />
+                        <Input
+                            name="password"
+                            icon={FiLock}
+                            type="password"
+                            placeholder="Login"
+                        />
+                        <Button type="submit"> Create account </Button>
+                    </Form>
 
-                <a href="Login">
-                    <FiArrowLeft />
-                    Back to Logon
-                </a>
+                    <Link to="/">
+                        <FiArrowLeft />
+                        Back to Log in
+                    </Link>
+                </AnimationContainer>
             </Content>
         </Container>
     );
